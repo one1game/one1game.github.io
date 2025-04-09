76b257ccc4eaefd5c1a2d13d65a986fd10a4a4be
PhpDead <igoralx9119@gmail.com>
2025-04-09 22:43:53 +0500

    в

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 // Получаем элементы
 const playPauseBtn = document.getElementById('playPauseBtn');
-const stopBtn = document.getElementById('stopBtn');
 const audioPlayer = document.getElementById('radio-player');
 const bars = document.querySelectorAll('.bar');
 
@@ -17,13 +16,4 @@ playPauseBtn.addEventListener('click', () => {
         // Останавливаем анимацию эквалайзера
         bars.forEach(bar => bar.style.animationPlayState = 'paused');
     }
-});
-
-// Функция для остановки воспроизведения
-stopBtn.addEventListener('click', () => {
-    audioPlayer.pause();
-    audioPlayer.currentTime = 0; // сбросить время воспроизведения
-    playPauseBtn.textContent = 'Play';
-    // Останавливаем анимацию эквалайзера
-    bars.forEach(bar => bar.style.animationPlayState = 'paused');
 });
