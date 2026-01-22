ee7ba8f8be5b51c8a9e1c0433fd605b524e3a7bf
one1game <igoralx9119@gmail.com>
2026-01-23 00:08:13 +0500

    1

--- generate-sitemap.py
+++ generate-sitemap.py
@@ -1,27 +1,33 @@
 import re
 
-# Читаем ваш файл с данными
-with open('articles-data.js', 'r', encoding='utf-8') as f:
-    content = f.read()
+# Читаем данные из вашего файла со статьями
+try:
+    with open('articles-data.js', 'r', encoding='utf-8') as f:
+        content = f.read()
+except FileNotFoundError:
+    print("Файл articles-data.js не найден!")
+    exit(1)
 
-# Ищем все ссылки в поле "url"
-urls = re.findall(r'"url":\s*"([^"]+)"', content)
-urls += re.findall(r'url:\s*"([^"]+)"', content)
+# Ищем все ссылки (поддерживаем оба формата записи в JS)
+urls = re.findall(r'url:\s*["\']([^"\']+)["\']', content)
 
-# Начало XML
-sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n'
-sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-sitemap += '  <url><loc>https://one1game.github.io/</loc><priority>1.0</priority></url>\n'
-sitemap += '  <url><loc>https://one1game.github.io/archive.html</loc><priority>0.8</priority></url>\n'
+# Формируем XML
+xml_content = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    '  <url><loc>https://one1game.github.io/</loc><priority>1.0</priority></url>',
+    '  <url><loc>https://one1game.github.io/archive.html</loc><priority>0.8</priority></url>'
+]
 
-# Добавляем статьи
 for url in urls:
-    # Убираем лишний слэш в начале, если он есть
+    # Очищаем ссылку от начального слэша, чтобы не было двойного //
     clean_url = url.lstrip('/')
-    sitemap += f'  <url><loc>https://one1game.github.io/{clean_url}</loc><priority>0.7</priority></url>\n'
+    xml_content.append(f'  <url><loc>https://one1game.github.io/{clean_url}</loc><priority>0.7</priority></url>')
 
-sitemap += '</urlset>'
+xml_content.append('</urlset>')
 
-# Сохраняем результат
+# Записываем поверх вашего пустого sitemap.xml
 with open('sitemap.xml', 'w', encoding='utf-8') as f:
-    f.write(sitemap)
+    f.write('\n'.join(xml_content))
+
+print("Sitemap успешно обновлен!")
